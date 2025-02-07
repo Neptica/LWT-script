5bcefc5abbd5ae9331fa600bf8c7cdd411e5f6e7
Michael Mumme <neptica@hotmail.com>
2025-02-06 19:37:21 -0600

    kinda working

---
+++ dl-clean.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 output language URL"
+    echo "Additional information: Output destination must exclude file extension and lang must be two character code -- (en, de, fr)"
+    exit 1
+fi
+
+current_dir=$(pwd)
+
+output_file="$current_dir/$1"
+lang="$2"
+url="$3"
+
+temp_file="$current_dir/temp1234567890.vtt"
+
+# set -e
+yt-dlp --write-auto-subs --sub-langs "$lang" --skip-download -o "$output_file" "$url"
+
+touch "$temp_file"
+output_file="$output_file.$lang.vtt"
+
+sed -E '
+    /^WEBVTT$/d;
+    /^Kind:/d;
+    /^Language:/d;
+    s/<[0-9:.]+>//g;
+    s/<\/?c>//g;
+    /^[0]/d;
+
+    /^[[:space:]]*$/d;
+
+' "$output_file" > "$temp_file"
+
+sed 'n;s/.*/ /' "$temp_file" > "$output_file"
+sed 'N; /^\(.*\)\n\1$/d; N; /^\(.*\)\n\1$/d; P; D' "$temp_file" > "$output_file"
+
+rm -rf "$tempfile"
+
+echo "Clean script saved at the following destination: $output_file"
