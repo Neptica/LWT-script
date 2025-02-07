badbb5964eec0ee7c4347825816aaf942e26e115
Michael Mumme <neptica@hotmail.com>
2025-02-06 20:25:58 -0600

    add check for native subs first
    If no native subs found auto generated subs will be downloaded instead

--- dl-clean.sh
+++ dl-clean.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+underline() { echo -e "\033[1m$1\033[0m"; }
+
 if [[ $# -ne 3 ]]; then
     echo "Usage: $0 output language URL"
-    echo "Additional information: Output destination must exclude file extension and lang must be two character code -- (en, de, fr)"
+    underline "Additional information:"
+    echo "Output destination must exclude file extension"
+    echo "Language must be two character code (en, de, fr)"
     exit 1
 fi
 
@@ -15,12 +19,19 @@ url="$3"
 temp_file="$current_dir/temp1234567890.vtt"
 
 # set -e
-yt-dlp --write-auto-subs --sub-langs "$lang" --skip-download -o "$output_file" "$url"
 
-touch "$temp_file"
-output_file="$output_file.$lang.vtt"
 
-sed -E '
+echo "Checking available subtitles in $lang..."
+response=$(yt-dlp --write-subs --sub-langs "$lang" --skip-download -o "$output_file" "$url" 2>&1)  # Capture command output, including errors
+
+if [[ $response == *"[info] There are no subtitles for the requested languages"* ]]; then
+    echo "No subtitles found. Downloading auto generated subtitles..."
+
+    response=$( yt-dlp --write-auto-subs --sub-langs "$lang" --skip-download -o "$output_file" "$url" 2>&1) 
+    touch "$temp_file"
+    output_file="$output_file.$lang.vtt"
+
+    sed -E '
     /^WEBVTT$/d;
     /^Kind:/d;
     /^Language:/d;
@@ -30,10 +41,27 @@ sed -E '
 
     /^[[:space:]]*$/d;
 
-' "$output_file" > "$temp_file"
+    ' "$output_file" > "$temp_file"
 
-python3 rdupes.py "$temp_file" "$output_file"
+    python3 rdupes.py "$temp_file" "$output_file"
+else
+    echo "Subtitles found! Processing file..."
 
-rm -rf "$tempfile"
+    touch "$temp_file"
+    output_file="$output_file.$lang.vtt"
 
-echo "Clean script saved at the following destination: $output_file"
+    sed -E '
+    /^WEBVTT$/d;
+    /^Kind:/d;
+    /^Language:/d;
+    /^[0]/d;
+
+    ' "$output_file" > "$temp_file"
+
+    python3 strip-native.py "$temp_file" "$output_file"
+fi
+
+
+rm -rf "$temp_file"
+
+echo "File saved to: $output_file"
---
+++ strip-native.py
@@ -0,0 +1,24 @@
+import sys
+
+temp = sys.argv[1]
+output = sys.argv[2]
+
+fixed = []
+working = ""
+for line in open(temp, "r", encoding="UTF-8"):
+    if line == "\n":
+        continue
+
+    line = line.strip()
+    if line[-1] == ".":
+        working += line
+        fixed.append(working)
+        working = ""
+    elif working != "":
+        working += " " + line
+    else:
+        working += line
+
+
+fixed_output = "\n".join(fixed)
+open(output, "w", encoding="UTF-8").write(fixed_output)
